becb224f35074a005f1f8ca49fe99ae469541dbb
Szymon Hrabia <35381167+Survikrowa@users.noreply.github.com>
2023-07-27 13:57:48 +0200

    feat/remove-supabase-ts

--- services/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://clewygpyfjeeepgwjepq.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsZXd5Z3B5ZmplZWVwZ3dqZXBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk3NTQzMzAsImV4cCI6MjAwNTMzMDMzMH0.avGqB8rvW6BQ3p7h_JJDBnEX0i6FzyONXNZRNSDMb0M";
-const supabase = createClient(supabaseUrl, supabaseKey);
